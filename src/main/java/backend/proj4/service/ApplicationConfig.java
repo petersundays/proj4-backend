8667a7599f279104341e43579509e5bb82497b46
Pedro Jorge Figueiredo Domingos <152998554+petersundays@users.noreply.github.com>
2024-03-11 01:16:02 +0000

    configuração BD

---
+++ src/main/java/backend/proj4/service/ApplicationConfig.java
@@ -0,0 +1,8 @@
+package backend.proj4.service;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/rest")
+public class ApplicationConfig extends Application {
+}
